96c66e32b6b1711ca1d7b105cfa63f0321e96b5f
Tomás Duarte <git@tomasduarte.eu>
2026-06-01 14:22:46 +0100

    Add a dev/debug proxy for /media

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 from django.views.generic import RedirectView
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path("", RedirectView.as_view(url="/pt/", permanent=False)),
     path("", include("pages.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
