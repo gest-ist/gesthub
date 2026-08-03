987edb8e33530c8d3a75bb9ce9db1c2e2c298a09
Guam7734 <guam7734@gmail.com>
2026-08-03 15:12:47 +0100

    Changed migrations

--- inventory/migrations/0001_initial.py
+++ inventory/migrations/0001_initial.py
@@ -121,7 +121,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('item_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='inventory.item')),
                 ('bgg_id', models.PositiveIntegerField(blank=True, null=True, unique=True)),
-                ('year', models.PositiveSmallIntegerField(blank=True, null=True)),
+                ('year', models.SmallIntegerField(blank=True, null=True)),
                 ('weight', models.DecimalField(blank=True, decimal_places=2, max_digits=3, null=True)),
                 ('min_players', models.PositiveSmallIntegerField(blank=True, null=True)),
                 ('max_players', models.PositiveSmallIntegerField(blank=True, null=True)),
